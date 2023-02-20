950df19520db34a36b4c7d267668ebe3045952ec
nitin <nimble2905@gmail.com>
2023-02-21 02:05:32 +0530

    page js

---
+++ script.js
@@ -0,0 +1,103 @@
+let x = document.querySelector(".MyDiv");
+function myFunction() {
+  x.classList.toggle("menudiv")
+//   x.classList.toggle("answer")
+  }
+  
+let a = document.querySelector(".answer1");
+function myAnswer1() {
+    console.log("fuck you");
+//   a.classList.toggle("menudiv")
+  a.classList.toggle("answerdiv")
+  }
+
+let b = document.querySelector(".answer2");
+function myAnswer2() {
+    console.log("fuck you");
+//   a.classList.toggle("menudiv")
+  b.classList.toggle("answerdiv")
+  }
+
+let c = document.querySelector(".answer3");
+function myAnswer3() {
+    console.log("fuck you");
+//   a.classList.toggle("menudiv")
+  c.classList.toggle("answerdiv")
+  }
+
+let d = document.querySelector(".answer4");
+function myAnswer4() {
+    console.log("fuck you");
+//   a.classList.toggle("menudiv")
+  d.classList.toggle("answerdiv")
+  }
+
+let e = document.querySelector(".answer5");
+function myAnswer5() {
+    console.log("fuck you");
+//   a.classList.toggle("menudiv")
+  e.classList.toggle("answerdiv")
+  }
+
+let f = document.querySelector(".answer6");
+function myAnswer6() {
+    console.log("fuck you");
+//   a.classList.toggle("menudiv")
+  f.classList.toggle("answerdiv")
+  }
+
+let g = document.querySelector(".answer7");
+function myAnswer7() {
+    console.log("fuck you");
+//   a.classList.toggle("menudiv")
+  g.classList.toggle("answerdiv")
+  }
+
+let p = document.querySelector(".answer8");
+function myAnswer8() {
+    console.log("fuck you");
+//   a.classList.toggle("menudiv")
+  p.classList.toggle("answerdiv")
+  }
+
+let q = document.querySelector(".answer9");
+function myAnswer9() {
+    console.log("fuck you");
+//   a.classList.toggle("menudiv")
+  q.classList.toggle("answerdiv")
+  }
+
+let r = document.querySelector(".answer10");
+function myAnswer10() {
+    console.log("fuck you");
+//   a.classList.toggle("menudiv")
+  r.classList.toggle("answerdiv")
+  }
+
+let s = document.querySelector(".answer11");
+function myAnswer11() {
+    console.log("fuck you");
+//   a.classList.toggle("menudiv")
+  s.classList.toggle("answerdiv")
+  }
+
+let t = document.querySelector(".answer12");
+function myAnswer12() {
+    console.log("fuck you");
+//   a.classList.toggle("menudiv")
+  t.classList.toggle("answerdiv")
+  }
+
+let u = document.querySelector(".answer13");
+function myAnswer13() {
+    console.log("fuck you");
+//   a.classList.toggle("menudiv")
+  u.classList.toggle("answerdiv")
+  }
+
+let v = document.querySelector(".answer14");
+function myAnswer14() {
+    console.log("fuck you");
+//   a.classList.toggle("menudiv")
+  v.classList.toggle("answerdiv")
+  }
